f08e4788a68c1a158d826832d500f4eaa4080fb0
Gavin Steinhoff <GavinSteinhoff@gmail.com>
2023-10-02 23:07:26 -0500

    Subscribe and unsubscribe functionality

--- lib/screens/organization_screen.dart
+++ lib/screens/organization_screen.dart
@@ -16,8 +16,11 @@ class OrganizationScreen extends StatefulWidget {
 
 class _OrganizationScreenState extends State<OrganizationScreen> {
   late QueryDocumentSnapshot<Object?> _document;
-  final db = FirebaseFirestore.instance;
-  final FirebaseAuth auth = FirebaseAuth.instance;
+  Future<DocumentSnapshot<Map<String, dynamic>>> _userFuture = FirebaseFirestore
+      .instance
+      .collection("users")
+      .doc(FirebaseAuth.instance.currentUser?.uid)
+      .get();
 
   @override
   void initState() {
@@ -37,15 +40,61 @@ class _OrganizationScreenState extends State<OrganizationScreen> {
         children: [
           Text(_document.get("name")),
           Text(_document.get("description")),
-          ElevatedButton(
-              onPressed: () {
-                db.collection("users").doc(auth.currentUser?.uid).update({
-                  "subscriptions": FieldValue.arrayUnion([_document.id])
-                });
-              },
-              child: const Text("Subscribe"))
+          FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+              future: _userFuture,
+              builder: (BuildContext context,
+                  AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>>
+                      snapshot) {
+                if (!snapshot.hasData) {
+                  return const CircularProgressIndicator();
+                }
+                if (snapshot.hasError) {
+                  return const SizedBox.shrink();
+                }
+                print(snapshot.data?.get("subscriptions"));
+                if (snapshot.data
+                    ?.get("subscriptions")
+                    .contains(_document.id)) {
+                  return ElevatedButton(
+                      onPressed: () => subscribeButton(snapshot.data),
+                      child: const Text("Unsubscribe"));
+                } else {
+                  return ElevatedButton(
+                      onPressed: () => subscribeButton(snapshot.data),
+                      child: const Text("Subscribe"));
+                }
+              })
         ],
       ),
     )));
+  }
+
+  void subscribeButton(DocumentSnapshot<Map<String, dynamic>>? user) {
+    if (user == null) return;
+    final userDoc = FirebaseFirestore.instance
+        .collection("users")
+        .doc(FirebaseAuth.instance.currentUser?.uid);
+
+    if (user.get("subscriptions").contains(_document.id)) {
+      userDoc.update({
+        "subscriptions": FieldValue.arrayRemove([_document.id])
+      });
+      setState(() {
+        _userFuture = FirebaseFirestore.instance
+            .collection("users")
+            .doc(FirebaseAuth.instance.currentUser?.uid)
+            .get();
+      });
+    } else {
+      userDoc.update({
+        "subscriptions": FieldValue.arrayUnion([_document.id])
+      });
+      setState(() {
+        _userFuture = FirebaseFirestore.instance
+            .collection("users")
+            .doc(FirebaseAuth.instance.currentUser?.uid)
+            .get();
+      });
+    }
   }
 }
